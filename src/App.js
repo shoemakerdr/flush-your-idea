1011c94c2ee793126c022a26cb60530ab40157ef
Derek Shoemaker <shoemakerdr@gmail.com>
2018-04-08 12:24:17 -0500

    basic project structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import background from './images/flush-background.png'
+import goldfish from './images/goldfish.png'
 import './App.css';
 
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div class='wrapper'>
+                    <input className='ideaInput' />
+                    <div>
+                        <img className='backgroundImg' src={background} alt='background for bathroom'/>
+                    </div>
+                    <img className='fish' src={goldfish} alt='goldfish'/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
