120380165bc11323a9be70c32dd797612fc1f098
Derek Shoemaker <shoemakerdr@gmail.com>
2018-04-09 13:20:59 -0500

    Configured CSS modules and added water png

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import background from './images/flush-background.png'
 import goldfish from './images/goldfish.png'
-import './App.css';
+import toiletWater from './images/toilet-water.png'
+import styles from './App.css';
 
 
 
 class App extends Component {
+    state = {
+        title: 'flush your idea'
+    }
+
+    inputFocus = () => {
+        this.setState({
+            title: '',
+        })
+    }
+
     render() {
         return (
-            <div className="App">
-                <div class='wrapper'>
-                    <input className='ideaInput' />
+            <div className={styles.App}>
+                <div className={styles.wrapper}>
                     <div>
-                        <img className='backgroundImg' src={background} alt='background for bathroom'/>
+                        <img className={styles.backgroundImg} src={background} alt='background for bathroom'/>
                     </div>
-                    <img className='fish' src={goldfish} alt='goldfish'/>
+                    <div className={styles.title}>{this.state.title}</div>
+                    <input onClick={this.inputFocus} className={styles.ideaInput} />
+                    <div className={styles.subtitle}>it was sh*#t anyway</div>
+                    <div className={styles.toiletWaterWrapper}>
+                        <img className={styles.toiletWater} src={toiletWater} alt='toilet water'/>
+                    </div>
+                    <img className={styles.fish} src={goldfish} alt='goldfish'/>
                 </div>
             </div>
         );
